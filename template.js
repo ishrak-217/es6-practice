a37c1de2e1336b3f0605a88f5df0811f67471f84
Md. Ishrak Al Hasib Syeam <ishrakalhasib217@gmail.com>
2021-02-04 23:46:59 +0600

    learnt about template

---
+++ template.js
@@ -0,0 +1,17 @@
+const firstName = "Justin";
+const lastName = "Timberlake";
+const fullName = firstName + lastName;
+console.log(fullName);
+
+const fullName2 = `${firstName} ${lastName}`;
+console.log(fullName2);
+
+const multiLine = "I love you" +
+    "I miss you" +
+    "I need you";
+console.log(multiLine);
+
+const multiLine2 = `I love you
+I miss you
+I need you.`;
+console.log(multiLine2);
