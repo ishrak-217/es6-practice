949be026a9f6572d1a98c8cba9112f9c363f4c89
Md. Ishrak Al Hasib Syeam <ishrakalhasib217@gmail.com>
2021-02-04 23:28:48 +0600

    learnt about const let

---
+++ letconst.js
@@ -0,0 +1,12 @@
+const wife = "Samiha Sultana";
+console.log(wife);
+
+let clientName = "Rahim Chacha";
+clientName = "Karim Chacha";
+console.log(clientName);
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+}
+console.log(sum);
